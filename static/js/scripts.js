3d34cd335c398dc49fef1af4e5abb2aaa30431ca
ranaehelal <115326882+ranaehelal@users.noreply.github.com>
2024-09-17 20:07:30 +0300

    Add files via upload

---
+++ static/js/scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            } else {
+                entry.target.classList.remove('in-view');
+            }
+        });
+    });
+
+    observer.observe(document.querySelector('#about'));
+});
